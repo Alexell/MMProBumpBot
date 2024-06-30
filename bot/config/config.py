3e565efccea6d5d82dc52bc15ccf0f27261dfa90
Alexell <alexell@alexell.ru>
2024-06-30 15:01:13 +0500

    Using tabs for indentation

--- bot/config/config.py
+++ bot/config/config.py
@@ -2,15 +2,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
+	model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
 
-    API_ID: int
-    API_HASH: str
+	API_ID: int
+	API_HASH: str
 
-    CLAIM_RETRY: int = 3
-    SLEEP_BETWEEN_CLAIM: int = 180
+	CLAIM_RETRY: int = 3
+	SLEEP_BETWEEN_CLAIM: int = 180
 
-    USE_PROXY_FROM_FILE: bool = False
+	USE_PROXY_FROM_FILE: bool = False
 
 
 settings = Settings()
